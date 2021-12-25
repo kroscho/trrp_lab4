b4775f2f506e9d155255735a6574c3389d204191
AndreCSKA <andreirat00@gmail.com>
2021-12-25 12:50:31 +0500

    work client

---
+++ client/Lab2/Client.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using RabbitMQ.Client;
+using Server;
+using Newtonsoft.Json;
+using System.Configuration;
+using Grpc.Core;
+using System.Drawing;
+using Filter;
+using System.IO;
+using Google.Protobuf;
+
+
+namespace Client
+{
+    public class Client
+    {
+        static int port = int.Parse(ConfigurationManager.AppSettings.Get("Port"));
+        static string address = ConfigurationManager.AppSettings.Get("Adress");
+
+
+
+        public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
+            
+        }
+
+        public FilterService.FilterServiceClient ConnectServ()
+        {
+            try
+            {
+                Channel channel = new Channel(address + ":" + port, ChannelCredentials.Insecure);
+                var client = new DispatcherService.DispatcherServiceClient(channel);
+                var reply = client.GetFilterServer(new FilterServerRequest());
+                switch (Convert.ToInt64(reply.Error))
+                {
+                    case 0:
+                        channel = new Channel(reply.Address, ChannelCredentials.Insecure);
+                        var client1 = new FilterService.FilterServiceClient(channel);
+                        MessageBox.Show("Соединение установлено с сервером по адресу: " + reply.Address);
+                        return client1;
+                    case 1:
+                        MessageBox.Show("Серверы не найдены. Попробуйте подключиться позже.");
+                        return null;
+                    case 2:
+                        MessageBox.Show("");
+                        return null;
+                    case 3:
+                        MessageBox.Show("Сервер переполнен.");
+                        return null;
+                    case 4:
+                        MessageBox.Show("Сервер не найден.");
+                        return null;
+                    case 5:
+                        MessageBox.Show("Время ожидания вышло. Пока все серверы переполнены. Попробуйте подключиться позже.");
+                        return null;
+                    
+                }
+                MessageBox.Show("Неизвестная ошибка");
+                return null;
+
+            }
+            catch
+            {
+                MessageBox.Show("Не удается подключиться к диспетчеру");
+                return null;
+            }
+           
+        }
+
+        public System.Drawing.Bitmap SendgRPC(System.Drawing.Bitmap curImage, int fil, FilterService.FilterServiceClient client)
+        {
+                var data = imageToByteArray(curImage);
+                var reply = client.SendImage(new Filter.Image { Image_ = ByteString.CopyFrom(data), Type =fil });
+                Bitmap res=null;
+
+                using (var ms = new MemoryStream(reply.FilterImage.ToByteArray()))
+                {
+                    res = new Bitmap(Bitmap.FromStream(ms));
+                }
+
+                if(reply.Success)
+                     MessageBox.Show("Успешно");            
+                return res;
+
+
+
+        }
+    }
+}
---
+++ client/ServergRPC/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ServergRPC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ServergRPC")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3a620287-ae5f-4aa0-9208-3f3422bc5af3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
